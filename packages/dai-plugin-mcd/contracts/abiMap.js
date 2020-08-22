7712f3ca311983166047eaa70b357ce62ea5db84
Steven Enamakel <enamakel@cryptocontrol.io>
2020-08-23 04:38:04 +0530

    added adressess

--- packages/dai-plugin-mcd/contracts/abiMap.js
+++ packages/dai-plugin-mcd/contracts/abiMap.js
@@ -14,6 +14,7 @@ module.exports = {
   MCD_ADM: require('./abis/DSRoles.json'),
   MCD_CAT: require('./abis/Cat.json'),
   MCD_DAI: require('./abis/DSToken.json'),
+  MCD_ARTH: require('./abis/DSToken.json'),
   MCD_DAI_GUARD: require('./abis/DSGuard.json'),
   MCD_DEPLOY: require('./abis/DssDeploy.json'),
   MCD_FLAP: require('./abis/Flapper.json'),
